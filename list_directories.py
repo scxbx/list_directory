92467872db78fbcad501d8c34f36310e40134635
scxbx <761184937@qq.com>
2020-12-06 21:46:32 +0800

    list all directories(not files) in a directory

---
+++ list_directories.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os
+from openpyxl import Workbook
+
+# 打开文件
+dirs = os.listdir()
+real_dir_list = []
+for my_dir in dirs:
+    if os.path.isdir(my_dir):
+        real_dir_list.append(my_dir)
+
+book = Workbook()
+sheet = book.active
+
+for i in range(len(real_dir_list)):
+    sheet.cell(row=i+1, column=1).value = real_dir_list[i]
+book.save('list_directories.xlsx')
